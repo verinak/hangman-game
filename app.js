4c66189cc83d96e674295983333d1fee8377e78b
verinak <verinamichelk@gmail.com>
2026-02-07 05:49:06 +0200

    add hangman images and result calculation logic

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// console.log(DATA.flowers);
-
 var letters = "abcdefghijklmnopqrstuvwxyz";
 
 var answerWord;
 var category;
 
+var mistakes = 0;
+var lettersFound = 0;
+
 var lettersDiv = document.querySelector('div.letters');
 var answerDiv = document.querySelector('div#answer');
 var categoryDiv = document.querySelector('div.category');
+var hangmanImg = document.querySelector('.game-right img');
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -31,7 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
         if (answerWord[i] !== " ") {
             span.textContent = '_';
             span.id = 'char' + i;
-            span.addEventListener('click', onLetterClicked)
+        } else {
+            lettersFound++;  // balafa2 ?
         }
 
         answerDiv.insertAdjacentElement('beforeend', span)
@@ -42,21 +45,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function onLetterClicked(event) {
-    console.log(event.target.textContent);
-
+    // console.log(event.target.textContent);
     var found = false;
     for (var i = 0; i < answerWord.length; i++) {
         if (event.target.textContent === answerWord[i].toLowerCase()) {
             found = true;
+            lettersFound++;
             console.log('correct letter');
             document.getElementById('char' + i).textContent = answerWord[i];
         }
     }
     if (!found) {
         console.log('wrong letter');
+        mistakes++;
+        hangmanImg.src = './assets/hangman' + mistakes + '.png';
     }
 
-    event.target.classList.add('hidden');
+    // event.target.classList.add('hidden');
+    // disable button
+    event.target.classList.add('disabled');
+    event.target.removeEventListener('click', onLetterClicked);
 
+    // check round result
+    // console.log(mistakes);
+    // console.log(lettersFound);
+
+    if (mistakes === 6) {
+        console.log('lost!');
+        hangmanImg.src = './assets/hangman-lose.png';
+
+        endRound(true);
+    }
+
+    if (lettersFound === answerWord.length) {
+        // console.log(answerWord.length);
+        console.log('won!');
+        hangmanImg.src = './assets/hangman-win.png';
+
+        endRound(false)
+    }
+
+}
+
+function endRound(win) {
+
+    // disable all buttons
+    lettersDiv.querySelectorAll('span').forEach(function (span) {
+        span.classList.add('disabled');
+        span.removeEventListener('click', onLetterClicked);
+    })
 
 }
